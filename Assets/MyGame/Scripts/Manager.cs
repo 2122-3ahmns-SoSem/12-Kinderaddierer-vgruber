c488f22ea28241c036a266a0d37a516d1d598286
wiverite <wiverite@gmail.com>
2022-05-18 21:18:19 +0200

    SO all done

--- Assets/MyGame/Scripts/Manager.cs
+++ Assets/MyGame/Scripts/Manager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public enum MyScenes
 
@@ -17,14 +18,36 @@ public enum MyScenes
 
 public class Manager : MonoBehaviour
 {
+    public TMP_InputField ipfKidsName;
+    public TMP_Text displayKidsName;
+
+    private SoRuntimeData runtimeData;
+
+    private void Start()
+    {
+        runtimeData = Resources.Load<SoRuntimeData>("KinderaddiererRuntimeData");
+
+
+        if (SceneManager.GetActiveScene().buildIndex == (int)MyScenes.MathScene)
+        {
+            displayKidsName.text = runtimeData.nameKid;
+        }
+    }
+
     //generisch ueber Button 
 
     public void SwitchTheScene(int x)
-
     {
+        Debug.Log("button pressed, get kidsname " + ipfKidsName.text);
 
+
+        if (SceneManager.GetActiveScene().buildIndex == (int)MyScenes.WelcomeScene)
+        {
+            displayKidsName.text = runtimeData.nameKid;
+        }
+
+        runtimeData.nameKid = ipfKidsName.text;
         SceneManager.LoadScene(x);
-
     }
 
 
