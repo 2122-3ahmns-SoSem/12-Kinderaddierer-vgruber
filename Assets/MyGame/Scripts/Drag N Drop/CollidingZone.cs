b1d0337e4203c2716d2a180693cd343ffb6705f0
wiverite <wiverite@gmail.com>
2022-06-12 19:55:04 +0200

    Drag And Drop Implemented in first Math Scene

---
+++ Assets/MyGame/Scripts/Drag N Drop/CollidingZone.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class CollidingZone : MonoBehaviour
+{
+    public Numbers refPoints;
+
+    public int correct, incorrect;
+
+
+    private Manager manager;
+
+    private void Awake()
+    {
+        manager = FindObjectOfType<Manager>();
+    }
+
+    public void Start()
+    {
+        correct = incorrect = 0;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("colission works ");
+        collision.transform.parent = gameObject.transform;
+        collision.transform.localPosition = Vector3.zero;
+
+        manager.SetAllNumbersDragable(false);
+
+
+       
+        if (refPoints.representingNumber == collision.GetComponent<Numbers>().representingNumber)
+        {
+            manager.AddToAndShowCorrectTasks();
+        }
+        else
+        {
+            manager.AddToAndShowIncorrectTasks();
+        }
+    }
+}
---
+++ Assets/MyGame/Scripts/Drag N Drop/Manager.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour
+{
+    public Numbers refNumberPoints;
+    public GameObject dropZone;
+    public GameObject parentSigns;
+    public Numbers[] nbrsPlain;
+    public int correct, incorrect;
+    public TMP_Text textCorrect, textIncorrect;
+    public TMP_Text displayKid;
+
+    private SoRuntimeData runtimeData;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateNewTask();
+        correct = incorrect = 0;
+        textCorrect.text = correct.ToString();
+        textIncorrect.text = incorrect.ToString();
+        runtimeData = Resources.Load<SoRuntimeData>("KinderaddiererRuntimeData");
+
+        displayKid.text = runtimeData.nameKid;
+    }
+
+    public void AddToAndShowCorrectTasks()
+    {
+        correct++;
+        textCorrect.text = correct.ToString();
+    }
+    public void AddToAndShowIncorrectTasks()
+    {
+        incorrect++;
+        textIncorrect.text = incorrect.ToString();
+    }
+
+    private void GenerateNewTask()
+    {
+        refNumberPoints.representingNumber = Random.Range(1, 10);
+        refNumberPoints.SetupNumberRepresentation();
+    }
+
+    public void ResetTask()
+    {
+        GenerateNewTask();
+        SetAllNumbersDragable(true);
+
+        for(int i = 0; i < nbrsPlain.Length; i++)
+        {
+            nbrsPlain[i].Reset(parentSigns);
+        }
+
+     
+    }
+
+    public void SetAllNumbersDragable(bool dragOk)
+    {
+        for(int i = 0; i< nbrsPlain.Length; i++)
+        {
+            nbrsPlain[i].dragable = dragOk;
+        }
+    }
+
+    public void SwitchTheScene(int x)
+    {
+        if (correct == 4)
+        {
+            SceneManager.LoadScene(x);
+        }
+    }
+}
